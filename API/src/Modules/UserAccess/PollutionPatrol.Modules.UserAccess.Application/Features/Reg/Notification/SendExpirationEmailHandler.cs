5f7d157f870201ef085cc3729e5e5963ae2f7156
Ilia <ShigeraIlia@gmail.com>
2024-03-23 10:13:09 -0400

    UserAccess: implement verification and expiration email sending

--- API/src/Modules/UserAccess/PollutionPatrol.Modules.UserAccess.Application/Features/Reg/Notification/SendExpirationEmailHandler.cs
+++ API/src/Modules/UserAccess/PollutionPatrol.Modules.UserAccess.Application/Features/Reg/Notification/SendExpirationEmailHandler.cs
@@ -2,8 +2,35 @@ namespace PollutionPatrol.Modules.UserAccess.Application.Features.Reg.Notificati
 
 internal sealed class SendExpirationEmailHandler : IDomainEventHandler<RegistrationExpiredDomainEvent>
 {
-    public Task Handle(RegistrationExpiredDomainEvent domainEvent, CancellationToken cancellationToken)
+    private readonly ITemplateEngine _templateEngine;
+    private readonly IEmailSender _emailSender;
+
+    public SendExpirationEmailHandler(ITemplateEngine templateEngine, IEmailSender emailSender)
     {
-        return Task.CompletedTask;
+        _templateEngine = templateEngine;
+        _emailSender = emailSender;
+    }
+
+    public async Task Handle(RegistrationExpiredDomainEvent domainEvent, CancellationToken cancellationToken)
+    {
+        var body = GetBody(domainEvent.Username);
+
+        var email = new EmailMessage(
+            to: domainEvent.Email,
+            subject: "Verification Link Expired",
+            body);
+
+        await _emailSender.SendEmailAsync(email, cancellationToken);
+    }
+
+    private string GetBody(string username)
+    {
+        var templateModel = new ExpirationEmailModel
+        {
+            Username = username,
+        };
+
+        var template = _templateEngine.RenderTemplate(TemplateNames.RegistrationExpiredEmailTemplate, templateModel);
+        return template;
     }
 }
--- API/src/Modules/UserAccess/PollutionPatrol.Modules.UserAccess.Application/Features/Reg/Notification/SendVerificationEmailHandler.cs
+++ API/src/Modules/UserAccess/PollutionPatrol.Modules.UserAccess.Application/Features/Reg/Notification/SendVerificationEmailHandler.cs
@@ -2,8 +2,43 @@ namespace PollutionPatrol.Modules.UserAccess.Application.Features.Reg.Notificati
 
 internal sealed class SendVerificationEmailHandler : IDomainEventHandler<NewUserRegisteredDomainEvent>
 {
-    public Task Handle(NewUserRegisteredDomainEvent domainEvent, CancellationToken cancellationToken)
+    private readonly ITemplateEngine _templateEngine;
+    private readonly ILinkProvider _linkProvider;
+    private readonly IEmailSender _emailSender;
+
+    public SendVerificationEmailHandler(
+        ITemplateEngine templateEngine,
+        ILinkProvider linkProvider,
+        IEmailSender emailSender)
     {
-        return Task.CompletedTask;
+        _templateEngine = templateEngine;
+        _linkProvider = linkProvider;
+        _emailSender = emailSender;
+    }
+
+    public async Task Handle(NewUserRegisteredDomainEvent domainEvent, CancellationToken cancellationToken)
+    {
+        var body = GetBody(domainEvent.Username, domainEvent.VerificationCode);
+
+        var email = new EmailMessage(
+            to: domainEvent.Email,
+            subject: "Verify Your Pollution Patrol Account",
+            body);
+
+        await _emailSender.SendEmailAsync(email, cancellationToken);
+    }
+
+    private string GetBody(string username, string verificationCode)
+    {
+        var verificationLink = _linkProvider.GetRegistrationVerifyLink(verificationCode);
+
+        var templateModel = new VerificationEmailModel
+        {
+            Username = username,
+            Link = verificationLink,
+        };
+
+        var template = _templateEngine.RenderTemplate(TemplateNames.VerifyRegistrationEmailTemplate, templateModel);
+        return template;
     }
 }
